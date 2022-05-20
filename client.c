df2601def1348e07bf13312564238d8b597aaa85
bashaar1 <102796388+bashaar1@users.noreply.github.com>
2022-05-20 07:42:12 +0000

    UDP Iterative Code

--- client.c
+++ client.c
@@ -1,24 +1,22 @@
-#include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-#include <netinet/in.h>
+#include<netinet/in.h>
 #define port 8080
 int main()
 {
-    int sockfd;
-    sockfd = socket(PF_INET,SOCK_STREAM,0);
+    int sockfd  = socket(AF_INET,SOCK_DGRAM,0);
     struct sockaddr_in my;
     my.sin_addr.s_addr=INADDR_ANY;
     my.sin_port=htons(port);
-    my.sin_family=PF_INET;
-    int len = (sizeof(my));
-    connect(sockfd,(struct sockaddr *)&my,len);
-    char buffer[20];
-    int n =recv(sockfd,buffer,20,0);
-    buffer[n]='\0';
-    printf("%s",buffer);
-    send(sockfd,"Ma Theak Tum sunao\n",19,0);
+    my.sin_family=AF_INET;
+    int len = sizeof(my);
+    sendto(sockfd,"KAISA HO SERVER",16,0,(struct sockaddr *)&my,len);
+    char buff[49];
+    recvfrom(sockfd,buff,48,MSG_WAITALL,(struct sockaddr *)&my,len);
+    printf("\n%s",buff);
     close(sockfd);
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,29 +1,25 @@
-#include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<netinet/in.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-#include <netinet/in.h>
-//server Code TCP
 #define port 8080
 int main()
 {
-    int sockfd;
-    sockfd=socket(PF_INET,SOCK_STREAM,0);
-    struct sockaddr_in my;
+    int sockfd = socket(PF_INET,SOCK_DGRAM,0);
+    struct sockaddr_in my,clientaddr;
     my.sin_addr.s_addr=INADDR_ANY;
     my.sin_family=PF_INET;
     my.sin_port=htons(port);
-    bind(sockfd,(struct sockaddr *)&my,sizeof(my));
-    listen(sockfd,10);
-    struct sockaddr_in newclientdata;
-    int len=sizeof(struct sockaddr_in);
-    int clientsfd=accept(sockfd,(struct sockaddr *)&newclientdata,&len);
-    send(clientsfd,"Client Kaisa ha?",17,0);
+    int len = sizeof(my);
+    bind(sockfd,(struct sockaddr*) &my,len);
     char buffer[1024];
-    int n=recv(clientsfd,buffer,1024,0);
+    len=sizeof(clientaddr);
+    int n=recvfrom(sockfd,buffer,1024,MSG_WAITALL,(struct sockaddr *)&clientaddr,&len);
     buffer[n]='\0';
-    printf("Clinet Response To server : %s\n",buffer);
+    printf("Data Coming from client: %s\n",buffer);
+    sendto(sockfd,"Ma theak chalo ma chalta hoon baad ma baat hoti",48,0,(struct sockaddr * )&my,len);
     close(sockfd);
     return 0;
 }
